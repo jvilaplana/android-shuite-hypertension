507816837ca4bca4777803eeae38f868439579b8
ancarrera <ancarrera31@gmail.com>
2015-02-02 03:08:48 +0100

    working in init Activity

--- app/src/main/java/com/android/bpcontrol/InitActivity.java
+++ app/src/main/java/com/android/bpcontrol/InitActivity.java
@@ -1,9 +1,14 @@
 package com.android.bpcontrol;
 
-import android.content.Context;
+
 import android.os.Bundle;
-import android.os.Handler;
+
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.android.bpcontrol.application.BPcontrolMasterActivity;
 import com.android.bpcontrol.utils.LogBP;
@@ -21,7 +26,11 @@ public class InitActivity extends BPcontrolMasterActivity {
     public void onCreate(Bundle onInstanceState){
         super.onCreate(onInstanceState);
         setContentView(R.layout.initactivitylayout);
-        getActionBar().hide();
+        View view = getActionBarView();
+        view.findViewById(R.id.textviewbpcontrol).setVisibility(View.GONE);
+        view.findViewById(R.id.fakeInit).setVisibility(View.INVISIBLE);
+        view.findViewById(R.id.actionBarMenu).setVisibility(View.INVISIBLE);
+        view.findViewById(R.id.imageinit).setVisibility(View.VISIBLE);
 
 
     }
@@ -82,9 +91,30 @@ public class InitActivity extends BPcontrolMasterActivity {
 
         public SlidingImages(ImageView image, int currentImagePosition) {
 
-            image.setAn;
+
             this.image = image;
             this.counter = currentImagePosition;
+            addDimensions();
+            addAnimation();
+
+        }
+
+        private void addDimensions() {
+
+            DisplayMetrics displayMetrics = InitActivity.this.getResources().getDisplayMetrics();
+
+            float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+            float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+
+            float imagensizeHeight = (dpHeight *300) / 592;
+            float imagensizeWidth = (dpWidth * 300) / 360;
+            image.getLayoutParams().height = (int)(imagensizeHeight * displayMetrics.density);
+            image.getLayoutParams().width = (int)(imagensizeWidth * displayMetrics.density);
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)image.getLayoutParams();
+            params.setMargins((int)(30*displayMetrics.density),(int)(25*displayMetrics.density),(int)(30*displayMetrics.density),0);
+            image.setLayoutParams(params);
 
         }
 
@@ -112,6 +142,13 @@ public class InitActivity extends BPcontrolMasterActivity {
 
         public int getCounter() {
             return counter;
+        }
+
+        private void addAnimation(){
+
+            final Animation anim = AnimationUtils.loadAnimation(InitActivity.this,R.anim.fade_in);
+            image.setAnimation(anim);
+
         }
     }
 
